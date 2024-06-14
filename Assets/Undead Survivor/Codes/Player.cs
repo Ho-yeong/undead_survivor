5c2cbdf3322d1441f66b02646b8e864dc1416365
simon <ghdud0503@gmail.com>
2024-06-14 12:39:43 +0900

    Update: character animation

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -10,24 +10,28 @@ public class Player : MonoBehaviour
     public float speed;
     
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // // Update is called once per frame
     // 기존 방식
-    // void Update()
-    // {
-    //     inputVec.x = Input.GetAxisRaw("Horizontal");
-    //     inputVec.y = Input.GetAxisRaw("Vertical");
-    // }
-
-    // input system 방식
-    void OnMove(InputValue value) {
-        inputVec = value.Get<Vector2>();
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
     }
+
+    // // input system 방식
+    // void OnMove(InputValue value) {
+    //     inputVec = value.Get<Vector2>();
+    // }
 
     void FixedUpdate() {
         // 힘을 준다.
@@ -39,5 +43,14 @@ public class Player : MonoBehaviour
         // 위치 이동
         Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    // Update가 끝난이후 적용
+    void LateUpdate() {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
