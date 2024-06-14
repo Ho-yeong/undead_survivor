dee86b54da0d6d8cc425fbd7cf217e44bb5740cf
simon <ghdud0503@gmail.com>
2024-06-14 23:03:10 +0900

    Update: Infinity map

--- Assets/Undead Survivor/Codes/Reposition.cs
+++ Assets/Undead Survivor/Codes/Reposition.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void OnTriggerExit2D(Collider2D collision) {
+        if (!collision.CompareTag("area")) {
+            return;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+
+        float diffX = Mathf.Abs(dirX);
+        float diffY = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+        switch(transform.tag) {
+            case "ground":
+                if (diffX > diffY) {
+                    transform.Translate(Vector3.right * dirX * 40);
+                } else if (diffX < diffY) {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "enemy": 
+                break;
+        }
     }
 }
