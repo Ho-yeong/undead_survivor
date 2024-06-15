5d6fa589b0cc10fecaa01058def7c053e37aa7b4
simon <ghdud0503@gmail.com>
2024-06-16 01:19:56 +0900

    standoff weapon

--- Assets/Undead Survivor/Codes/Bullet.cs
+++ Assets/Undead Survivor/Codes/Bullet.cs
@@ -7,8 +7,32 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
-    public void Init(float damage, int per) {
+    Rigidbody2D rigid;
+
+    void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(float damage, int per, Vector3 dir) {
         this.damage = damage;
         this.per = per;
+
+        // 원거리
+        if (per > -1) {
+            rigid.velocity = dir * 15f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (!collision.CompareTag("Enemy") || per == -1) {
+            return;
+        }    
+
+        per--;
+
+        if (per == -1) {
+            rigid.velocity = Vector3.zero;
+            gameObject.SetActive(false);
+        }
     }
 }
--- Assets/Undead Survivor/Codes/Weapon.cs
+++ Assets/Undead Survivor/Codes/Weapon.cs
@@ -10,6 +10,13 @@ public class Weapon : MonoBehaviour
     public int count;
     public float speed;
 
+    float timer;
+    Player player;
+
+    void Awake() {
+        player = GetComponentInParent<Player>();
+    }
+
     void Start() {
         Init();
     }
@@ -20,12 +27,18 @@ public class Weapon : MonoBehaviour
                 transform.Rotate(Vector3.back * speed * Time.deltaTime);
                 break;
             default:
+                timer += Time.deltaTime;
+
+                if (timer > speed) {
+                    timer = 0;
+                    Fire();
+                }
                 break;
         }
 
         // .. Test Code ..
         if (Input.GetButtonDown("Jump")) {
-            LevelUp(20, 5);
+            LevelUp(10, 1);
         }
     }
 
@@ -41,10 +54,13 @@ public class Weapon : MonoBehaviour
     public void Init() {
         switch (id) {
             case 0:
+                // 회전속도
                 speed = -150;
                 Batch();
                 break;
             default:
+                // 연사속도
+                speed = 0.3f;
                 break;
         }
     }
@@ -67,7 +83,23 @@ public class Weapon : MonoBehaviour
             Vector3 rotVec = Vector3.forward * 360 * i / count;
             bullet.Rotate(rotVec);
             bullet.Translate(bullet.up * 1.5f, Space.World);
-            bullet.GetComponent<Bullet>().Init(damage, -1); // -1 is Infinity per
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero); // -1 is Infinity per
         }
+    }
+
+    void Fire() {
+        if (!player.scanner.nearestTarget) {
+            return;
+        }
+
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir = targetPos - transform.position;
+        // 크기는 1로, 방향만 남기는 값으로 변경
+        dir = dir.normalized;
+
+        Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        bullet.GetComponent<Bullet>().Init(damage, count, dir);
     }
 }
