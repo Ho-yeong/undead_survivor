e21c153d38584ae7028b813450262fb31b2cedf3
simon <ghdud0503@gmail.com>
2024-06-16 17:18:58 +0900

    HUD'

--- Assets/Undead Survivor/Codes/GameManager.cs
+++ Assets/Undead Survivor/Codes/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
     public float maxGameTime = 2 * 10f;
 
     [Header("# Player Info")]
+    public int maxHealth = 100;
+    public int health;
     public int level;
     public int kill;
     public int exp;
@@ -24,6 +26,9 @@ public class GameManager : MonoBehaviour
         instance = this;
     }
 
+    void Start() {
+        health = maxHealth;
+    }
 
     void Update()
     {
